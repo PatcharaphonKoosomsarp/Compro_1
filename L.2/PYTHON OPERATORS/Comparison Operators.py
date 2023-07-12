2edb80e7f91f3fc3046cf31ea1eddb2c6ff79aae
PatcharaphonKoosomsarp <s6506022620095@email.kmutnb.ac.th>
2023-07-12 09:46:59 +0700

    update

---
+++ L.2/PYTHON OPERATORS/Comparison Operators.py
@@ -0,0 +1,15 @@
+x = 10
+y = 12
+
+# Output: x > y is False
+print('x > y is',x>y)
+# Output: x < y is True
+print('x < y is',x<y)
+#Output: x == y is False
+print('x == y is',x==y)
+#Output: x != y is True
+print('x != y is',x!=y)
+#Output: X >= y is False
+print('x >= y is',x>=y)
+#Output: x <= y is True
+print('x <= y is',x<=y)
