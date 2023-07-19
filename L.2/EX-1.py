5153e427522c2a7614ef24ea92fa2028db5f27b6
PatcharaphonKoosomsarp <s6506022620095@email.kmutnb.ac.th>
2023-07-19 10:29:16 +0700

    update

---
+++ L.2/EX-1.py
@@ -0,0 +1,9 @@
+print('Students Score')
+first_score = int(input('First Score :'))
+second_score = int(input('Second Score :'))
+third_score = int(input('Third Score :'))
+average = (first_score+second_score+third_score)/3
+if average > 95:
+    print('congratulations')
+print(average)
+
