617b613d90ba42c851ad1a5f57998da54ad1d73f
PatcharaphonKoosomsarp <s6506022620095@email.kmutnb.ac.th>
2023-07-19 10:41:26 +0700

    update

---
+++ L.2/EX-3.py
@@ -0,0 +1,11 @@
+ScoreTest = int(input("Enter Score Test : "))
+if ScoreTest >= 90:
+    print("You grade is A")
+elif ScoreTest >= 80:
+    print("You grade is B")
+elif ScoreTest >= 70:
+    print("You grade is C")
+elif ScoreTest >= 60:
+    print("You grade is D")
+else:
+    print("You grade is F")
