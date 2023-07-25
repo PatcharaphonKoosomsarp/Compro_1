c410b0121fa573e8f7cf51836c4f0dd56e6ce6f0
PatcharaphonKoosomsarp <s6506022620095@email.kmutnb.ac.th>
2023-07-25 17:59:59 +0700

    update

--- L.3/while2.py
+++ L.3/while2.py
@@ -1,14 +1,13 @@
-sales = float(input('Enter the amount of sales: '))
-comm_rate = float(input('Enter the commission rate: '))
-commission = sales * comm_rate
-print('The commission is $', format(commission, ',.2f'))
+keep_going = 'y'
 
-sales = float(input('Enter the amount of sales: '))
-comm_rate = float(input('Enter the commission rate: '))
-commission = sales * comm_rate
-print('The commission is $', format(commission, ',.2f'))
+while keep_going == 'y':
 
-sales = float(input('Enter the amount of sales: '))
-comm_rate = float(input('Enter the commission rate: '))
-commission = sales * comm_rate
-print('The commission is $', format(commission, ',.2f'))
+    sales = float(input('Enter the amount of sales: '))
+    comm_rate = float(input('Enter the commission rate: '))
+
+    commission = sales * comm_rate
+
+    print('The commision is $',format(commission, ',.2f'), sep='')
+
+    keep_going = input('Do you want to calculate another ' + \
+                        'commission (Enter y for yes): ')
