8e3282225f99197569c4dae3d33763a2009fc9c4
PatcharaphonKoosomsarp <s6506022620095@email.kmutnb.ac.th>
2023-07-26 10:01:02 +0700

    update

--- L.3/sentinel-1.py
+++ L.3/sentinel-1.py
@@ -1,6 +1,6 @@
 go = 'y'
 while go == 'y':
-    wholesale = int(input("Enter the item's wholesale cost: "))
+    wholesale = float(input("Enter the item's wholesale cost: "))
     retail = wholesale * 2.5
     print("Retail price: $", retail)
     go = input("Do you have another item? (Enter y for yes): ")
