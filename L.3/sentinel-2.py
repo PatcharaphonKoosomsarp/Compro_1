8c4866b02fbac329481b56e15810848d86d627e8
PatcharaphonKoosomsarp <s6506022620095@email.kmutnb.ac.th>
2023-07-26 10:14:39 +0700

    update

--- L.3/sentinel-2.py
+++ L.3/sentinel-2.py
@@ -1,6 +1,6 @@
-num_row = int(input('How many rows ?'))
-num_col = int(input('How many columns ?'))
-for now in range(num_row):
-    for now in range(num_col):
+num_row = int(input('How many rows? '))
+num_col = int(input('How many columns? '))
+for i in range(num_row):
+    for i in range(num_col):
         print('*', end='')
     print("")
