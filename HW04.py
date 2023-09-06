4aad363e27c46424c36e681dce67baeaacd8c1d5
Patcharaphon <s6506022620095@email.kmutnb.ac.th>
2023-09-06 10:20:59 +0700

    update

--- HW04.py
+++ HW04.py
@@ -1,15 +1,13 @@
 def find_peak_indices(nums):
-    peak_indices = [i for i in range(1, len(nums) - 1) if nums[i] > nums[i - 1] and nums[i] > nums[i + 1]]
-    return peak_indices
+    return [i for i in range(1, len(nums) - 1) if nums[i] > nums[i - 1] and nums[i] > nums[i + 1]]
+
+def print_peak_indices(nums, peak_indices):
+    print(f"Peak element index of: {nums} -> {peak_indices}")
 
 nums1 = [3, 2, 3, 1]
 nums2 = [1, 2, 1, 3, 5, 6, 4]
 nums3 = [1, 2, 1, 4, 5, 6, 4, 7]
 
-peak_indices1 = find_peak_indices(nums1)
-peak_indices2 = find_peak_indices(nums2)
-peak_indices3 = find_peak_indices(nums3)
-
-print(f"Peak element index of {nums1} -> {peak_indices1}")
-print(f"Peak element index of {nums2} -> {peak_indices2}")
-print(f"Peak element index of {nums3} -> {peak_indices3}")
+print_peak_indices(nums1, find_peak_indices(nums1))
+print_peak_indices(nums2, find_peak_indices(nums2))
+print_peak_indices(nums3, find_peak_indices(nums3))
